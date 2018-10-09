d35a386f1f40955f14731c56e876dc6db554c709
Yiheng <HenryQuan@users.noreply.github.com>
2018-10-09 20:52:04 +1100

    basic server (remember to add custom port)

--- winserver/winserver/Program.cs
+++ winserver/winserver/Program.cs
@@ -1,14 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Net;
+using System;
+using System.Threading;
 using System.Text;
 
 namespace winserver
 {
     class Program
     {
+
+        static HttpListener _httpListener = new HttpListener();
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Starting server...");
+            _httpListener.Prefixes.Add("http://192.168.1.102:8605/"); // add prefix "http://localhost:5000/"
+            _httpListener.Start(); // start server (Run application as Administrator!)
+            Console.WriteLine("Server started.");
+            Thread _responseThread = new Thread(ResponseThread);
+            _responseThread.Start(); // start the response thread
+        }
+
+        static void ResponseThread()
+        {
+            while (true)
+            {
+                HttpListenerContext context = _httpListener.GetContext(); // get a context
+                                                                          // Now, you'll find the request URL in context.Request.Url
+                byte[] _responseArray = Encoding.UTF8.GetBytes("<html><head><title>Localhost server -- port 5000</title></head>" +
+                "<body>Welcome to the <strong>Localhost server</strong> -- <em>port 5000!</em></body></html>"); // get the bytes to response
+                context.Response.OutputStream.Write(_responseArray, 0, _responseArray.Length); // write bytes to the output stream
+                context.Response.KeepAlive = false; // set the KeepAlive bool to false
+                context.Response.Close(); // close the connection
+                Console.WriteLine("Respone given to a request.");
+            }
         }
     }
 }
