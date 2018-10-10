4612bcfa667f7940a2fa83aa8495c2a8099c3803
Yiheng <HenryQuan@users.noreply.github.com>
2018-10-10 21:10:05 +1100

    add port to firewall

--- winserver/winserver/Program.cs
+++ winserver/winserver/Program.cs
@@ -2,37 +2,81 @@
 using System;
 using System.Threading;
 using System.Text;
+using NetFwTypeLib;
+using System.Diagnostics;
 
 namespace winserver
 {
     class Program
     {
-
-        static HttpListener _httpListener = new HttpListener();
+        static HttpListener listener = new HttpListener();
 
         static void Main(string[] args)
         {
+            // Add port first for foreign access
+            AddPortToFirewall();
+
+            var address = "http://192.168.1.102:8605/";
             Console.WriteLine("Starting server...");
-            _httpListener.Prefixes.Add("http://192.168.1.102:8605/"); // add prefix "http://localhost:5000/"
-            _httpListener.Start(); // start server (Run application as Administrator!)
-            Console.WriteLine("Server started.");
-            Thread _responseThread = new Thread(ResponseThread);
-            _responseThread.Start(); // start the response thread
+            // Add the address you want to use
+            listener.Prefixes.Add(address);
+            listener.Start(); // start server (Run application as Administrator!)
+            Console.WriteLine("Server is now online at " + address);
+            Process.Start(address);
+
+            var response = new Thread(ResponseThread);
+            response.Start(); // start the response thread
         }
 
         static void ResponseThread()
         {
             while (true)
             {
-                HttpListenerContext context = _httpListener.GetContext(); // get a context
-                                                                          // Now, you'll find the request URL in context.Request.Url
-                byte[] _responseArray = Encoding.UTF8.GetBytes("<html><head><title>Localhost server -- port 5000</title></head>" +
-                "<body>Welcome to the <strong>Localhost server</strong> -- <em>port 5000!</em></body></html>"); // get the bytes to response
-                context.Response.OutputStream.Write(_responseArray, 0, _responseArray.Length); // write bytes to the output stream
+                var context = listener.GetContext();
+                byte[] responseArray = Encoding.UTF8.GetBytes("Hello World From C#"); // get the bytes to response
+                context.Response.OutputStream.Write(responseArray, 0, responseArray.Length); // write bytes to the output stream
                 context.Response.KeepAlive = false; // set the KeepAlive bool to false
                 context.Response.Close(); // close the connection
-                Console.WriteLine("Respone given to a request.");
+                Console.WriteLine("200 OK");
             }
+        }
+
+        /// <summary>
+        /// Add port 8605 to windows firewall
+        /// </summary>
+        static void AddPortToFirewall()
+        {
+            try
+            {
+                Type TicfMgr = Type.GetTypeFromProgID("HNetCfg.FwMgr");
+                INetFwMgr icfMgr = (INetFwMgr)Activator.CreateInstance(TicfMgr);
+
+                // add a new port
+                Type TportClass = Type.GetTypeFromProgID("HNetCfg.FWOpenPort");
+                INetFwOpenPort portClass = (INetFwOpenPort)Activator.CreateInstance(TportClass);
+
+                // Get the current profile
+                INetFwProfile profile = icfMgr.LocalPolicy.CurrentProfile;
+
+                // Set the port properties
+                portClass.Scope = NetFwTypeLib.NET_FW_SCOPE_.NET_FW_SCOPE_ALL;
+                portClass.Enabled = true;
+                portClass.Protocol = NetFwTypeLib.NET_FW_IP_PROTOCOL_.NET_FW_IP_PROTOCOL_TCP;
+                // WoWs Info - 8605
+                portClass.Name = "WoWs Info";
+                portClass.Port = 8605;
+
+                // Add the port to the ICF Permissions List
+                profile.GloballyOpenPorts.Add(portClass);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Failed to add port to firewall. This is the error message.\n");
+                Console.WriteLine(e.Message);
+                Console.WriteLine("\nPlease feel free to open an issue to discuss this it with me.");
+                Process.Start("https://github.com/HenryQuan/WoWs-RS");
+            }
+            
         }
     }
 }
